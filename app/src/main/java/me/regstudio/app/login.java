d25060413784f73c2702c71e898843df9c76ab51
diegoportela99 <diegofportela1@gmail.com>
2018-03-21 14:35:19 +1100

    Login java

---
+++ app/src/main/java/me/regstudio/app/login.java
@@ -0,0 +1,18 @@
+package me.regstudio.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by diego on 3/21/2018.
+ */
+
+public class login extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
